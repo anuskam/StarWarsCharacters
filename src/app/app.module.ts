6ce4f07041ecaf2dc93eacb57287f8a7c2a200e8
Anna Moreta <anuskam92@gmail.com>
2024-06-17 12:10:22 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+// i18n
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslation } from './core/config/i18n/translate-loader.config';
+/* import { MultiLanguageComponent } from './components/multi-language/multi-language.component'; */
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TranslateModule,
+    HttpClientModule,
+  ],
+  providers: [importProvidersFrom(TranslateModule.forRoot(provideTranslation()))],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
