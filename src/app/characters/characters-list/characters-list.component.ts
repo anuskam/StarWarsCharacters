657da68b10ae4a6e031d3449bdcdec918bd7796d
Anna Moreta <anuskam92@gmail.com>
2024-06-17 23:32:50 +0200

    add service and print data

--- src/app/characters/characters-list/characters-list.component.ts
+++ src/app/characters/characters-list/characters-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { CharactersService } from '../../services/characters.service';
+import { ICharacter } from '../../interfaces/ICharacter.interface';
+/* import { ICharacter } from '../../interfaces/ICharacter.interface'; */
 @Component({
   selector: 'app-characters-list',
   templateUrl: './characters-list.component.html',
-  styleUrl: './characters-list.component.scss'
+  styleUrl: './characters-list.component.scss',
 })
-export class CharactersListComponent {
+export class CharactersListComponent implements OnInit {
+  public arrCharacters: ICharacter[] = [];
+  private charactersService = inject(CharactersService);
 
+  ngOnInit(): void {
+    /* this.charactersService.getCharacters().subscribe(
+      (character: ICharacter[]) => {
+        console.log('character', character);
+        this.arrCharacters = character;
+        console.log(this.arrCharacters);
+      },
+      error => {
+        console.error('Error al cargar personajes:', error);
+      },
+    ); */
+    this.charactersService
+      .getCharacters()
+      .then(data => {
+        this.arrCharacters = data; // Asigna los personajes obtenidos al arreglo characters
+        console.log(this.arrCharacters);
+      })
+      .catch(error => {
+        console.error('Error al cargar personajes:', error);
+      });
+  }
 }
--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ICharacter } from '../interfaces/ICharacter.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharactersService {
+  private starWarsUrl: string = 'assets/starWarsCharacters.json';
+  private httpClient = inject(HttpClient);
 
-  constructor() { }
+  /* getCharacters(): Observable<ICharacter[]> {
+    return this.httpClient
+      .get<ICharacter[]>(this.starWarsUrl)
+      .pipe(map((response: any) => response.characters));
+  } */
+  /* getCharacters(): Observable<ICharacter[]> {
+    const data = this.httpClient.get<ICharacter[]>(this.starWarsUrl);
+    console.log(data);
+    return data;
+  } */
+
+  /* getCharacters(): Observable<ICharacter[]> {
+    return this.httpClient
+      .get<{ characters: ICharacter[] }>(this.starWarsUrl)
+      .pipe(map(data => data.characters as ICharacter[]));
+  } */
+
+  getCharacters(): Promise<ICharacter[]> {
+    return fetch(this.starWarsUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => data.characters as ICharacter[]) // Mapea la respuesta a un arreglo de ICharacter
+      .catch(error => {
+        console.error('Error fetching characters:', error);
+        return [];
+      });
+  }
 }
