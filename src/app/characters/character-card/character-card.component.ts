b8deee2b9e6b2105ff0e8fc1cec05b3a3e0bc40c
Anna Moreta <anuskam92@gmail.com>
2024-06-18 23:31:07 +0200

    add data in the cards and show all of them

--- src/app/characters/character-card/character-card.component.ts
+++ src/app/characters/character-card/character-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ICharacter } from '../../interfaces/ICharacter.interface';
 
 @Component({
   selector: 'app-character-card',
   templateUrl: './character-card.component.html',
-  styleUrl: './character-card.component.scss'
+  styleUrl: './character-card.component.scss',
 })
 export class CharacterCardComponent {
-
+  @Input() character: ICharacter | undefined;
 }
